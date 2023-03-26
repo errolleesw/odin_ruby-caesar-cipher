90a2698645725b268108324c3b03d1ede147add8
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-03-26 11:34:28 +1100

    Add test with non-alpha char

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -3,10 +3,15 @@ require_relative '../script/script.rb' # this is the path to the script to run.
 
 
 RSpec.describe 'Caesar Cipher tests' do
-  describe 'encrypt single word' do
-    it 'return the encrypted word "test" with  a caesar shift of 1' do
+  describe 'Caesar Cipher test' do
+    it 'return the encrypted word "test" with  a caesar shift of 5' do
       expected_output = "yjxy"
       expect(caesar_cipher("test", 5)).to eq(expected_output)
+    end
+
+    it 'string with non-alphanumeric characters "What a string!" with a caesar shift of 5' do
+      expected_output = "Bmfy f xywnsl!"
+      expect(caesar_cipher("What a string!", 5)).to eq(expected_output)
     end
   end
 end
